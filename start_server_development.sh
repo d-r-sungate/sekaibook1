f759dca4ff1fc9476530d1998e164b5ed501794d
drsungate <>
2018-12-23 00:42:56 +0900

    サーバ起動をhttps化対応

--- start_server_development.sh
+++ start_server_development.sh
@@ -8,5 +8,6 @@ export RAILS_ENV=development
 
 
 # 開発サーバ起動
-bundle exec rails s -b 0.0.0.0
+#bundle exec rails s -b 0.0.0.0
 
+bundle exec rails s Puma -b 'ssl://0.0.0.0:3000?key=server.key&cert=server.crt'#
