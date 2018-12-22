701fca1350ffc40b31cd851d124096be42f9138c
drsungate <>
2018-12-23 00:43:28 +0900

    DBマイグレーション・シード追加

--- db/migrate/20181221003332_add_columns_to_users3.rb
+++ db/migrate/20181221003332_add_columns_to_users3.rb
@@ -1,5 +1,9 @@
 class AddColumnsToUsers3 < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :image, :string
+    add_column :users, :introduction, :string
+    add_column :users, :oganization, :string
+    add_column :users, :title, :string
+    add_column :users, :country, :string
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create(description: '海外就職')
+Category.create(description: 'キャリア')
+Category.create(description: '留学/MBA')
+Category.create(description: '移住')
+Category.create(description: '現地情報')
+Category.create(description: '語学留学')
+Category.create(description: '異文化/人間関係')
+Category.create(description: '国内のグローバル化')
+
+Area.create(description: 'グローバル')
+Area.create(description: 'アジア')
+Area.create(description: '北アメリカ')
+Area.create(description: '南アメリカ')
+Area.create(description: 'ヨーロッパ')
+Area.create(description: 'アフリカ')
+Area.create(description: 'オセアニア')
+Area.create(description: '日本')
